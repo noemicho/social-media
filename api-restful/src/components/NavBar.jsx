099f4e617c08f0df587e20864ffa4d14c8c95aaf
Noemi Cho de Almeida <noemicho14@gmail.com>
2024-08-27 14:27:47 -0300

    construindo menu (navBar)

--- api-restful/src/components/NavBar.jsx
+++ api-restful/src/components/NavBar.jsx
@@ -1,7 +1,19 @@
+import homeIcon from '../images/home-icon.png'
+import addPostIcon from '../images/add-post-icon.png'
+import profileIcon from '../images/profile-icon.png'
+import '../styles/NavBar.css'
+
+import {Link} from 'react-router-dom'
+
 export function NavBar(){
     return(
         <>
-            <p>post, like, perfil</p>
+            <div className='menu'>
+                <Link to='/'><img src={homeIcon} className='hover' alt="home-icon"/></Link>
+                <Link to='/addPost'> <img src={addPostIcon}  className='hover' alt="add-post-icon"/></Link>
+                <Link to='/profile'><img src={profileIcon} className='hover' alt="profile-icon"/></Link>       
+            </div>
+
         </>
     )
 }
