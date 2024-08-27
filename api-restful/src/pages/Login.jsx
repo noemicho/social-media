bc4173f5146a474b2dc28bcb7a8ea287127a5d51
Noemi Cho de Almeida <noemicho14@gmail.com>
2024-08-27 15:45:09 -0300

    pagina de login

--- api-restful/src/pages/Login.jsx
+++ api-restful/src/pages/Login.jsx
@@ -1,17 +1,21 @@
 import { NavBar } from "../components/NavBar";
 import {Link} from 'react-router-dom';
-import logo from '../images/logo-camera.png'
-import '../styles/Login.css'
+import logo from '../images/logo.png';
+import '../styles/Login.css';
 
 export function Login(){
     return(
         <>
             <div className="login">
                 <div className="logo-div">
-                    <Link to='/home'><img src={logo} className="logo"/></Link>
+                    <img src={logo} className="logo"/>
                 </div>
-                <div>
-                    
+                <div className="info-login">
+                    <input type='text' className="input-login" placeholder='Enter your username'/>
+                    <input type='password' className="input-login" placeholder='Password'/>
+
+                    <Link to='/home'><button className="login-button">Login</button></Link>
+                    <Link to='/register'><p className="register-button">Register</p></Link>
                 </div>
             </div>
         </>
