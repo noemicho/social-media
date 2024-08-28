203fbbf7f5f09bd12359c7f0223bb76eb6b34b5d
Noemi Cho de Almeida <noemicho14@gmail.com>
2024-08-28 16:06:07 -0300

    configurando mongodb

--- api-restful/backend/app.js
+++ api-restful/backend/app.js
@@ -1,9 +1,18 @@
 import express from 'express'
 import cors from 'cors'
-const app = express()
+import conn from './db/config_db.js'
 
+const app = express()
 app.use(cors())
 app.use(express.json())
+
+conn()
+
 app.listen(3002, function(){
     console.log('Escutando a porta 3002 do backend!')
 })
+
+//otQaCsGxyJkPwN5d
+//admin
+
+
---
+++ api-restful/backend/db/config_db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+//mongodb+srv://admin:otQaCsGxyJkPwN5d@cluster0.yllwi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+async function main(){
+    try{
+        await mongoose.connect('mongodb+srv://admin:otQaCsGxyJkPwN5d@cluster0.yllwi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    }
+    catch(error){
+        console.log(`Erro: ${error}`)
+    }
+}
+
+export default main;
