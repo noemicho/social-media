4a26c73e7d16417f24d7b0ce250c99ee0907f646
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-08-28 23:19:01 -0300

    aaaaa

--- api-restful/src/pages/Login.jsx
+++ api-restful/src/pages/Login.jsx
@@ -1,22 +1,43 @@
 import { NavBar } from "../components/NavBar";
+import { useNavigate } from 'react-router-dom'; // Importar useNavigate
 import {Link} from 'react-router-dom';
+import { useState } from "react";
 import logo from '../images/logo.png';
 import '../styles/Login.css';
 
 export function Login(){
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(username, password);
+        console.log("Envio");
+
+        navigate('/home');
+
+    };
+
     return(
         <>
             <div className="login">
+               <form onSubmit={handleSubmit}>
                 <div className="logo-div">
                     <img src={logo} className="logo"/>
                 </div>
                 <div className="info-login">
-                    <input type='text' className="input-login" placeholder='Enter your username'/>
-                    <input type='password' className="input-login" placeholder='Password'/>
+                    <input type='text' className="input-login" placeholder='Enter your username' onChange={(e) => setUsername(e.target.value)}/>
+                    <input type='password' className="input-login" placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
 
-                    <Link to='/home'><button className="login-button">Login</button></Link>
+                    <button type="submit" className="login-button">Login</button>
                     <Link to='/register'><p className="register-button">Register</p></Link>
                 </div>
+                </form>
+                
             </div>
         </>
     )
