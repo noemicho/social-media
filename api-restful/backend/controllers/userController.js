89f2a925291248f2ed70c4aad374a650e51e01ee
Noemi Cho de Almeida <noemicho14@gmail.com>
2024-08-29 11:22:25 -0300

    crud de user finalizado

--- api-restful/backend/controllers/userController.js
+++ api-restful/backend/controllers/userController.js
@@ -17,6 +17,50 @@ const userController = {
         }catch (error){
             console.log(error)
         }
+    },
+    getAll: async (request, response) => {
+        try{
+            const userAll = await User.find()
+
+            response.json(userAll)
+
+        }catch (error){
+            console.log(error)
+        }
+    },
+    get: async (request, response) => {
+        try{
+            const id = request.params.id
+            const user = await User.findById(id)
+            response.json(user)
+
+            if(!user){
+                response.status(404).json({msg: 'Não foi encontrado o usuário com esse Id'})
+                return
+            }
+
+        }catch (error){
+            console.log(error)
+        }
+    },
+    delete: async (request, response) => {
+        try{
+            const id = request.params.id
+            const user = await User.findById(id)
+            response.json(user)
+
+            if(!user){
+                response.status(404).json({msg: 'Não foi encontrado o usuário com esse Id'})
+                return
+            }
+
+            const deletedUser = await User.findByIdAndDelete(id)
+
+            response.status(200).json({deletedUser, msg: 'Usuário excluído com sucesso'})
+
+        }catch (error){
+            console.log(error)
+        }
     }
 }
 
--- api-restful/backend/routes/users.js
+++ api-restful/backend/routes/users.js
@@ -4,6 +4,8 @@ const router = express.Router();
 import userController from '../controllers/userController.js';
 
 router.route('/user').post((request, response) => userController.create(request, response))
-
+router.route('/user').get((request, response) => userController.getAll(request, response))
+router.route('/user/:id').get((request, response) => userController.get(request, response))
+router.route('/user/:id').delete((request, response) => userController.delete(request, response))
 
 export default router
