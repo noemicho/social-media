9610fe35bad41bc4bee97478b8f2290a77263574
FeixeDeLuz <luiz.mendescastro@gmail.com>
2024-08-29 16:11:17 -0300

    Protótipo 1 page Perfil

--- api-restful/frontend/src/pages/Profile.jsx
+++ api-restful/frontend/src/pages/Profile.jsx
@@ -6,9 +6,17 @@ export function Profile(){
     return(
         <div class="tudo">
             <img src={profileIcon} class="profilePic" alt="profile-icon"/>
-           
-
-
+            <div class="info">
+                <input type="text" class="campo"></input>
+                <input type="text" class="campo"></input>
+                <input type="text" class="campo"></input>
+           </div>
+           <div class ="photos">
+                <p class="photo">Photo 1</p>
+                <p class="photo">Photo 2</p>
+                <p class="photo">Photo 3</p>
+                <p class="photo">Photo 4</p>
+           </div>
             <NavBar />
         </div>
     )
