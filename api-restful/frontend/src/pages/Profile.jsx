b1ff01fdc4e0ea7cb1d00fece4319a3bfa857b23
FeixeDeLuz <luiz.mendescastro@gmail.com>
2024-08-31 11:43:17 -0300

    css do profile pt 1
    -noemi rs

--- api-restful/frontend/src/pages/Profile.jsx
+++ api-restful/frontend/src/pages/Profile.jsx
@@ -1,22 +1,33 @@
 import { NavBar } from "../components/NavBar.jsx";
-import { Post } from "../components/Post.jsx"
-import profileIcon from "../images/profile-icon.png"
 import "../styles/Profile.css"
 
 export function Profile(){
     return(
-        <div className="tudo">
-            <img src={profileIcon} className="profilePic" alt="profile-icon"/>
-            <div className="info">
-                <input type="text" className="campo"></input>
-                <input type="text" className="campo"></input>
-                <input type="text" className="campo"></input>
-           </div>
-           <p>Photo 1</p>
-           <p>Photo 2</p>
-           <p>Photo 3</p>
-           <p>Photo 4</p>
-            <NavBar />
-        </div>
+        <>
+            <div className="profile">
+                <div className="header"> 
+                    <h1 id='title-profile'>Profile</h1>
+                    <button id='edit-profile' onClick="">Edit Profile</button>
+                    <button id='logout' onClick="">Logout</button>
+                </div>
+
+                <div className="info">
+                    <input type="text" className="campo"></input>
+                    <input type="text" className="campo"></input>
+                    <input type="text" className="campo"></input>
+                </div>
+
+                <div className="posts-div">
+                <p>Photo 1</p>
+                <p>Photo 2</p>
+                <p>Photo 3</p>
+                <p>Photo 4</p>
+                </div>
+
+
+
+                <NavBar />
+            </div>
+        </>
     )
 }
